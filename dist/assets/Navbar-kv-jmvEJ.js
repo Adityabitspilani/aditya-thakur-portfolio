2a349aa7d1f1ddff26f4f60f185a894af4700cd7
Aditya Thakur <2025em1100345@bitspilani-digital.edu.in>
2026-03-24 02:44:13 +0530

    Add base path for GitHub Pages

--- dist/assets/Navbar-kv-jmvEJ.js
+++ dist/assets/Navbar-kv-jmvEJ.js
@@ -1,4 +1,4 @@
-import{j as S,r as Be}from"./index-BOBSvnXD.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j as S,r as Be}from"./index-Dej6WIUk.js";import{g as Ae,S as xe}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.12.7
  * https://gsap.com
  * 
